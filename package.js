ffcaf4fbbae712d0c6602c26046358e223cbf23b
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:25:34 -0500

    Updated component to version 1.9.0

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-loader',
+  summary : 'Semantic UI - Loader (official): Single component release of loader',
+  version : '1.9.0',
+  git     : 'git://github.com/Semantic-Org/UI-Loader.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
